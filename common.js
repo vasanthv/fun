49b665122352342c86b2393378ead22c6ac8d4a1
vasanth <imvasanthv@gmail.com>
2026-08-15 09:06:32 -0400

    favicon fix

--- common.js
+++ common.js
@@ -21,3 +21,21 @@ function initShareButton(getShareData) {
     }
   });
 }
+
+(function () {
+  var link = document.querySelector('link[rel="icon"]');
+  var dark = window.matchMedia("(prefers-color-scheme: dark)");
+  var paint = function () {
+    link.href =
+      "data:image/svg+xml," +
+      encodeURIComponent(
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">' +
+          '<path fill="' +
+          (dark.matches ? "#fff" : "#000") +
+          '" d="M90 40L50 80L10 40L30 20L50 40L70 20L90 40Z"/>' +
+          "</svg>",
+      );
+  };
+  dark.addEventListener("change", paint);
+  paint();
+})();
